62addd9425d1d14324b0e52ae5940ad7419e4ff3
Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
2025-03-20 23:31:29 +0200

    Update ProductRepo.kt

--- app/src/main/java/com/example/productsappwithmvvm/data/repo/ProductRepo.kt
+++ app/src/main/java/com/example/productsappwithmvvm/data/repo/ProductRepo.kt
@@ -1,10 +1,11 @@
 package com.example.productsappwithmvvm.data.repo
 
 import com.example.productsappwithmvvm.data.models.Product
+import kotlinx.coroutines.flow.Flow
 
 interface ProductRepo {
 
-    suspend fun getAllProducts(isOnline: Boolean): List<Product>?
+    suspend fun getAllProducts(isOnline: Boolean): Flow<List<Product>>?
 
     suspend fun addProduct(product: Product): Long
 
