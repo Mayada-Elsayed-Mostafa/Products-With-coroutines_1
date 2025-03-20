7a9a176973086f43fefbc97e98f938735e4ee881
Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
2025-03-20 23:31:01 +0200

    Update ProductRemoteDataSourceImp.kt

--- app/src/main/java/com/example/productsappwithmvvm/data/remote/ProductRemoteDataSourceImp.kt
+++ app/src/main/java/com/example/productsappwithmvvm/data/remote/ProductRemoteDataSourceImp.kt
@@ -1,9 +1,12 @@
 package com.example.productsappwithmvvm.data.remote
 
 import com.example.productsappwithmvvm.data.models.Product
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class ProductRemoteDataSourceImp(private val service: ProductService) : ProductRemoteDataSource {
-    override suspend fun getAllProducts(): List<Product>? {
-        return service.getProducts().body()?.products
+    override suspend fun getAllProducts(): Flow<List<Product>>? {
+        val response = service.getProducts().body()
+        return response?.products?.let { flow { emit(it) } }
     }
 }
