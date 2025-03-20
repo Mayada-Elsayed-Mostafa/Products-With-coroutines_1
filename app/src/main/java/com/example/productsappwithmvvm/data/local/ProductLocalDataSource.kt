7a1acd12a31361aae28dc9e9e6377c4e46450ad0
Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
2025-03-20 23:29:40 +0200

    Update ProductLocalDataSource.kt

--- app/src/main/java/com/example/productsappwithmvvm/data/local/ProductLocalDataSource.kt
+++ app/src/main/java/com/example/productsappwithmvvm/data/local/ProductLocalDataSource.kt
@@ -1,10 +1,11 @@
 package com.example.productsappwithmvvm.data.local
 
 import com.example.productsappwithmvvm.data.models.Product
+import kotlinx.coroutines.flow.Flow
 
 interface ProductLocalDataSource {
 
-    suspend fun getFavProducts(): List<Product>
+    suspend fun getFavProducts(): Flow<List<Product>>
 
     suspend fun insertProduct(product: Product): Long
 
