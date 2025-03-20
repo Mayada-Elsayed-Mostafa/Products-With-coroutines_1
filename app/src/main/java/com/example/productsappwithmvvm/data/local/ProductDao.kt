38b8791f1edd95c968e45ec4b5d8664c25e5d6c1
Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
2025-03-20 23:29:10 +0200

    Update ProductDao.kt

--- app/src/main/java/com/example/productsappwithmvvm/data/local/ProductDao.kt
+++ app/src/main/java/com/example/productsappwithmvvm/data/local/ProductDao.kt
@@ -6,13 +6,14 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.productsappwithmvvm.data.models.Product
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface ProductDao {
 
     @Query("SELECT * FROM producttable")
-    fun getFavoriteProducts(): List<Product>
+    fun getFavoriteProducts(): Flow<List<Product>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertProduct(product: Product): Long
