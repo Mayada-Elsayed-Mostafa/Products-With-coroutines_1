d65026f30a5dcec770b48945d63ed319de672027
Mayada Elsayed <140763552+Mayada-Elsayed-Mostafa@users.noreply.github.com>
2025-03-20 23:30:37 +0200

    Update ProductRemoteDataSource.kt

--- app/src/main/java/com/example/productsappwithmvvm/data/remote/ProductRemoteDataSource.kt
+++ app/src/main/java/com/example/productsappwithmvvm/data/remote/ProductRemoteDataSource.kt
@@ -1,8 +1,9 @@
 package com.example.productsappwithmvvm.data.remote
 
 import com.example.productsappwithmvvm.data.models.Product
+import kotlinx.coroutines.flow.Flow
 
 interface ProductRemoteDataSource {
 
-    suspend fun getAllProducts(): List<Product>?
+    suspend fun getAllProducts(): Flow<List<Product>>?
 }
